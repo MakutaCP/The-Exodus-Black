5467c1021d52869f09ed34d64c9e6fe3a7635454
MakutaCP <makutacp@gmail.com>
2020-07-30 17:38:14 -0400

    added javascript to form

--- tickets.js
+++ tickets.js
@@ -4,6 +4,48 @@ let ranNumb = Math.floor(Math.random() * 10000) +1;
 
 const sb = document.getElementById("submit");
  sb.addEventListener('click', function (event) {
-   alert (ranNumb);
+   alert ("Your ticket number is " + ranNumb);
 });
 
+function responseNumb() {
+  alert("Checking your ticket");
+  let tickNumber = document.getElementById("ticketNumb").value;
+  if (tickNumber < 0){
+    alert("Invalid ticket number. Please try again.");
+  }
+  if (tickNumber > 5000){
+    alert("Invalid ticket number. Please try again.");
+  }
+  if (tickNumber > 4500 && tickNumber < 5000){
+    alert("Flight has been delayed indefinatly due to cockpit failure.");
+  }
+  if (tickNumber > 4000 && tickNumber < 4500){
+    alert("Flight delayed 3754.39 years.");
+  }
+  if (tickNumber > 3500 && tickNumber < 4000){
+    alert("Flight is ready to board in one week.");
+  }
+  if (tickNumber > 3000 && tickNumber < 3500){
+    alert("Your flight has been delayed due to bad weather. We are planning a new launch day now.");
+  }
+  if (tickNumber > 2500 && tickNumber < 3000) {
+    alert("Your flight has been delayed three months. Your seating is unchanged.");
+  }
+  if (tickNumber > 2000 && tickNumber < 2500){
+    alert("Flight left 30 minutes ago.");
+  } 
+  if (tickNumber > 1500 && tickNumber < 2000){
+    alert("Launch date moved forward two weeks.");
+  }
+  if (tickNumber > 1000 && tickNumber < 1500){
+    alert("All systems ready for tomorrows launch!");
+  }
+  if (tickNumber > 0 && tickNumber < 1000){
+    alert("Flight in progress.");
+  }
+  else {
+    alert("You are the last survivor of the human race. Thank you for giving us your location.")
+  }
+}
+
+document.getElementById("submit_2").onclick = responseNumb;
