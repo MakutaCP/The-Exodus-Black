015b94138d9a8e18fae387fefe9ff23f2254c31b
MakutaCP <makutacp@gmail.com>
2020-07-30 21:14:12 -0400

    Final edits

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # The-Exodus-Black
 Website project for Code Louisville
 
+Note: Uncrompress zip folder before opening otherwise css and java do not load properly.
+To run open index.html
+
+Inspired by the recent spaceX launch my webcite is about the intersteller flight of a ship to colonize mars. 
+Most of the content is non-specific images of space and text. Some text provded by my little brother when I left the computer on when I went to work. Images from the NASA webcite.
+
 -Index has the most drastic changes due to css.
   --Flexbox to rearrange 2 or more content areas on your page as the screen changes sizes.
   --Media queries to switch from a one-column mobile layout to a two and three coumn desktop layout.
 
 -About has the working image carousel with at least three images.
 
--Tickets has the form that gives a random number and a finished java function that uses that number for a function.
+-Tickets has the form that gives a random number and a finished java function that uses that number for another function that gives several diferent answers based on the nubmber.
--- tickets.js
+++ tickets.js
@@ -12,35 +12,25 @@ function responseNumb() {
   let tickNumber = document.getElementById("ticketNumb").value;
   if (tickNumber > 4500 && tickNumber < 5000){
     alert("Flight has been delayed indefinatly due to cockpit failure.");
-  }
-  if (tickNumber > 4000 && tickNumber < 4500){
+  } else if (tickNumber > 4000 && tickNumber < 4500){
     alert("Flight delayed 3754.39 years.");
-  }
-  if (tickNumber > 3500 && tickNumber < 4000){
+  } else if (tickNumber > 3500 && tickNumber < 4000){
     alert("Flight is ready to board in one week.");
-  }
-  if (tickNumber > 3000 && tickNumber < 3500){
+  } else if (tickNumber > 3000 && tickNumber < 3500){
     alert("Your flight has been delayed due to bad weather. We are planning a new launch day now.");
-  }
-  if (tickNumber > 2500 && tickNumber < 3000) {
+  } else if (tickNumber > 2500 && tickNumber < 3000) {
     alert("Your flight has been delayed three months. Your seating is unchanged.");
-  }
-  if (tickNumber > 2000 && tickNumber < 2500){
+  } else if (tickNumber > 2000 && tickNumber < 2500){
     alert("Flight left 30 minutes ago.");
-  } 
-  if (tickNumber > 1500 && tickNumber < 2000){
+  } else if (tickNumber > 1500 && tickNumber < 2000){
     alert("Launch date moved forward two weeks.");
-  }
-  if (tickNumber > 1000 && tickNumber < 1500){
+  } else if (tickNumber > 1000 && tickNumber < 1500){
     alert("All systems ready for tomorrows launch!");
-  }
-  if (tickNumber > 0 && tickNumber < 1000){
+  } else if (tickNumber > 0 && tickNumber < 1000){
     alert("Flight in progress.");
-  }
-  if (tickNumber < 0 || tickNumber > 5000){
+  } else if (tickNumber < 0 || tickNumber > 5000){
     alert("Invalid ticket number. Please try again.");
-  }
-  else {
+  } else {
     alert("You are the last survivor of the human race. Thank you for giving us your location.")
   }
 }
